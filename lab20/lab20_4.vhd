c9613e4b7aa57dcc014088713d81b6b7321cf973
Sanerins <ASKosnitskiy@gmail.com>
2021-02-09 16:46:36 +0300

    Жопота несусветная
    Пиздосная работка

--- lab20/lab20_4.vhd
+++ lab20/lab20_4.vhd
@@ -14,46 +14,68 @@ ENTITY lab20_4 IS
 END lab20_4;
 
 ARCHITECTURE behav OF lab20_4 IS
-type byte is array (7 downto 0) of std_logic_vector(7 downto 0);
+type byte is array (8 downto 0) of std_logic_vector(7 downto 0);
 SIGNAL buff: byte;
 BEGIN
 process(DI, WR, RD)
-variable count: integer range -7 to 7;
-variable increase: integer;
-variable decrease: integer;
+variable count: integer range 8 to 0;
+variable inc: integer range 8 to 0;
+variable dec: integer range 8 to 0;
 begin
 
-count := increase - decrease;
+count := inc - dec;
 
 if(count = 0) then
 	FULL <= '0';
 	EMPTY <= '1';
-elsif(count = 7) then
+elsif(count = 8) then
 	FULL <= '1';
 	EMPTY <= '0';
 else
 	FULL <= '0';
 	EMPTY <= '0';
 end if;
-if(reset = '1') then
-	increase := 0;
+
+if(Reset = '1') then
+	inc := 0;
 elsif(rising_edge(WR)) then
-	if(count = 7) then
-		buff(7) <= DI;
+	if(count = 8) then
+		buff <= buff;
 	else
 		buff(count) <= DI;
-		increase := increase + 1;
+		inc := inc + 1;
 	end if;
 end if;
 
-if(reset = '1') then
-	decrease := 0;
+if(Reset = '1') then
+	dec := 0;
 elsif(rising_edge(RD)) then
 	if(count = 0) then
 		DO <= buff(0);
-	else
-		DO <= buff(count);
-		decrease := decrease + 1;
+	elsif(count = 8) then
+		DO <= buff(7);
+		dec := dec + 1;
+	elsif(count = 7) then
+		DO <= buff(6);
+		dec := dec + 1;
+	elsif(count = 6) then
+		DO <= buff(5);
+		dec := dec + 1;
+	elsif(count = 5) then
+		DO <= buff(4);
+		dec := dec + 1;
+	elsif(count = 4) then
+		DO <= buff(3);
+		dec := dec + 1;
+	elsif(count = 3) then
+		DO <= buff(2);
+		dec := dec + 1;
+	elsif(count = 2) then
+		DO <= buff(1);
+		dec := dec + 1;
+	elsif(count = 1) then
+		DO <= buff(0);
+		dec := dec + 1;
 	end if;
 end if;
 end process;
